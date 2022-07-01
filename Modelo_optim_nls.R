fca63c58576ab1557a4aecb2605c173ef40ce837
Chen Agrinvest <rafaelffchen@gmail.com>
2022-07-01 14:40:44 -0300

    Adjust logistic model

--- Modelo_optim_nls.R
+++ Modelo_optim_nls.R
@@ -17,6 +17,7 @@ acumulado <- dados %>%
           mutate(Y = cumsum(INSCRITOS)/100000) %>%
           ungroup()
 
+
 #Filtra para os canais
 canal_a <- acumulado %>%
             filter(CANAL == "vocesabia")
@@ -45,36 +46,6 @@ text(x = 425, y = f_log(DIAS = 400, L = 90, beta = 0.01, beta0 = 400),
 points(x = 400, pch = 19, col = "red",
        y = f_log(DIAS = 400, L = 90, beta = 0.01, beta0 = 400))
 
-###### MODELO DE OTIMIZAÇÃO ---------------------------------------------------- 
-#y = número de assinantes      
-f_ols <- function( par, dias, y) {
-        mu <- par[1]/(1 + exp(-par[2] * (dias - par[3])))
-        SQ <- sum( (y - mu)^2 )
-        return(SQ)
-  }
-
-xpar= c(46,-0.007,702)
-
-fit_logit_ols <- optim( par= xpar,
-                        fn = f_ols, 
-                        y = canal_a$Y, 
-                        dias = canal_a$DIAS)
-
-fit_logit_ols$par
-
-lst_day <- tail(canal_a,1)[1]%>%
-            pull() + 365
-
-DIAS <- seq(1:lst_day)
-
-plot(f_log(DIAS = DIAS, 
-           L = fit_logit_ols$par[1], 
-           beta = fit_logit_ols$par[3], 
-           beta0 = fit_logit_ols$par[3]) ~ DIAS, 
-     ylab = "Número de inscritos", 
-     xlab = "Dias da abertura",
-     type = "l")
-
 
 ############################# NLS #############################################
 ### CANAL vocesabia
@@ -90,13 +61,17 @@ n0 <- nls(Y ~ L/(1 + exp(B * (DIAS - M))),
 
 fit <- as.list(coef(n0))
 
+
 last_day <- tail(canal_a$DIAS, n=1) + 365
 
 nviews <- predict(n0, newdata = list(DIAS = last_day))
 
-subtitle <- sprintf("Número de inscritors %s milhões %s dias após a abertura ", 
+subtitle <- sprintf("Número de inscritors %s milhões %s dias após a abertura (L=%s, Beta=%s, Beta0=%s) ", 
                     round(nviews,2),
-                    last_day)
+                    last_day,
+                    round(coef(n0)[1],2),
+                    round(coef(n0)[3],2),
+                    round(coef(n0)[2],2))
 
 plot( formula = Y ~ DIAS, 
       data = canal_a,
@@ -120,10 +95,6 @@ points(x = last_day,
 
 
 ###### CANAL inventonahora
-fn_mk_projection <- function( canal = c('Canal A', 'Canal B'),
-                              dias_projecao = 365){
-
-  
 plot(Y ~ DIAS, data = canal_b )
 
 start <- list(L = 2000, M = 800, B = -0.01)
@@ -139,18 +110,24 @@ last_day <- tail(canal_b$DIAS, n=1) + 365
 
 nviews <- predict(n0, newdata = list(DIAS = last_day))
 
-subtitle <- sprintf("Número de inscritors %s milhões %s dias após a abertura ", 
+
+subtitle <- sprintf("Número de inscritors %s milhões %s dias após a abertura (L=%s, Beta=%s, Beta0=%s) ", 
                     round(nviews,2),
-                    last_day)
+                    last_day,
+                    round(coef(n0)[1],2),
+                    round(coef(n0)[3],2),
+                    round(coef(n0)[2],2))
+
 
 plot( formula = Y ~ DIAS, 
-         data = canal_b,
-         xlim = c(0, last_day + 50),
-         ylim = c(0, nviews + 5),
-         ylab = "Número de inscritos",
-         xlab = "Dias da abertura",
-         main ="Projeção de número de inscritos para os próximos 365 dias canal B",
-         sub = subtitle)
+      data = canal_b,
+      xlim = c(0, last_day + 50),
+      ylim = c(0, nviews + 5),
+      ylab = "Número de inscritos",
+      xlab = "Dias da abertura",
+      main ="Projeção de número de inscritos para os próximos 365 dias canal B",
+      sub = subtitle)
+
 
 with(fit,
      curve(L/(1 + exp(B * (x - M))),
@@ -162,5 +139,4 @@ points(x = last_day,
        pch = 35, 
        col = "red",
        y = nviews)
-}
-fn_mk_projection()
+
